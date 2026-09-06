810d42bea619c473e22f1068a035a67d1c1e1252
axwfae <axw.fae@gmail.com>
2026-09-06 08:09:17 +0800

    Delete src directory

--- src/fetch/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod client;
-pub mod cookies;
-pub mod decompress;
-pub mod guards;
-pub mod revalidate;
--- src/transport/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod h1;
-pub mod h2;
-pub mod pool;
-pub mod proxy;
-pub mod tcp;
-pub mod tls;
